85cdd2d3fd2f784da78412d52b8c42a8a0620e4d
mz <mzsjjwc@gmail.com>
2026-01-31 13:20:46 +0800

    1

---
+++ code/35.搜索插入位置.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=golang
+ * @lcpr version=30307
+ *
+ * [35] 搜索插入位置
+ */
+
+package main
+
+// @lc code=start
+func searchInsert(nums []int, target int) int {
+
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,3,5,6]\n5\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,3,5,6]\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,3,5,6]\n7\n
+// @lcpr case=end
+
+*/
